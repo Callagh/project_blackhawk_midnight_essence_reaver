0dc7e5cf0a586f6a435450d460db4f36a5d911d8
unknown <brendanswords21@gmail.com>
2026-03-09 23:28:00 +0000

    To add to my previous point, it was actually a Form component. Additionally I have removed some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Profile() {
   return (
     <Box>
       <Typography>Coming Soon, This was deployed automatically!</Typography>
-      <Form formData={{ id: "stuff", label: "things" }} />
     </Box>
   );
 }
